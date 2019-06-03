78f600d02425f066b4409fa39579859930a19f07
Mav <mavericktesini@gmail.com>
2019-06-02 20:06:36 -0400

    change usercardcontainer to have hook state

--- hooks_client/src/components/UserCardContainer.js
+++ hooks_client/src/components/UserCardContainer.js
@@ -1,23 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
 import UserCard from './UserCard';
 
-const UserCardContainer = ({ users }) => (
-  <div className="userCardContainer">
-    { users && users.map(({
-      name, info, isBig, id,
-    }) => (
-      <UserCard
-        key={id}
-        name={name}
-        info={info}
-        isBig={isBig}
-      />
-    ))
-    }
-  </div>
-);
+
+const UserCardContainer = ({ users }) => {
+  // initialize state with useState Hook
+  const [usersList, setUsersList] = useState([users]);
+
+  const fetchUsers = () => {
+    fetch('https://swapi.co/api/people/')
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Failed to fetch users');
+        }
+        return response.json();
+      })
+      .then((formattedResponse) => {
+        console.log('formatted resp: ', formattedResponse);
+        setUsersList(formattedResponse);
+      });
+  };
+
+  const handleClick = () => {
+    console.log('Fetching...');
+    return fetchUsers();
+  };
+
+  return (
+    <div className="userCardContainer">
+      <button type="button" onClick={handleClick}>Swap users to API</button>
+      { users && users.map(({
+        name, info, isBig, id,
+      }) => (
+        <UserCard
+          key={id}
+          name={name}
+          info={info}
+          isBig={isBig}
+        />
+      ))
+      }
+    </div>
+  );
+};
 
 export default UserCardContainer;
 
--- hooks_client/src/containers/Home.js
+++ hooks_client/src/containers/Home.js
@@ -1,31 +1,35 @@
 import React, { useState } from 'react';
-// importing Hook: useState
 import sampleUserArray from '../assets/SampleUserArray';
 import UserCardContainer from '../components/UserCardContainer';
 import './Home.css';
 
-const Home = (props) => {
+const Home = () => {
   /*
   * useState always returns an array with two elements: 1. Current State and 2. function() that overrides CS
   * Thus, we can array destructure when calling useState
   * Another diff is that Hook state does not need to be an object and can be set multiple times (diff states)
   * useState might require passing ...state because it overrides state
+  * Sample
+  * const [state, setState] = useState({
+  *   invisisbleInk: { name: 'Sleeper Agent' },
+  *   showUsers: false,
+  * });
   */
-  // const [state, setState] = useState({
-  //   invisisbleInk: { name: 'Super Spy' },
-  //   showUsers: false,
-  // });
-
   // Alternative:
-  const [invisisbleInk, setInvisibleInk] = useState('Super Spy');
+  const [invisisbleInk, setInvisibleInk] = useState('Sleeper Agent');
   const [showUsers, setShowUsers] = useState(false);
 
 
   const handleClick = () => {
-    // setState  with Hooks will overwrite state, so be careful. I pass state to accommodate
+    if (showUsers) {
+      setInvisibleInk('Deactivated');
+    } else {
+      setInvisibleInk('Activated');
+    }
     setShowUsers(!showUsers);
   };
 
+  console.log('Invisible Ink Reads: ', invisisbleInk);
   return (
     <div id="home">
       <h1>Home</h1>
