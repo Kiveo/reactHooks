5e8d96f83f6983d26726d9f8992c18019c1fc98a
Mav <mavericktesini@gmail.com>
2019-06-09 12:52:48 -0400

    improve re-rendering logic for attachment

--- hooks_client/src/containers/Attachment.js
+++ hooks_client/src/containers/Attachment.js
@@ -1,31 +1,32 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import Header from '../components/Header';
 
 const Attachment = ({ attachment }) => {
-  // BEGIN FILE READER
-  const imgRef = React.createRef();
-  const file = attachment;
-  const reader = new FileReader();
-  // ? setting up progress indicator
-  reader.addEventListener('progress', () => {
-    console.log('fr::PROGRESS: ', attachment.name);
-  }, true);
-
-  // once finished loading
-  reader.addEventListener('load', () => {
-    console.log('fr::LOADED: ', attachment.name);
-    imgRef.current.src = reader.result;
-  }, true);
-  if (file) {
-    reader.readAsDataURL(file);
-  }
-  // END FILE READER
+  // ? TRY TO SET STATE TO EVENT THROTTLE IMREF FILEREADER
+  const [imgRef] = useState(React.createRef());
+  // const imgRef = React.createRef();
 
   // TODO preventing re-renders like this may need adjusting after progress is implemented
   useEffect(() => {
     console.log('Effected: ', attachment.name);
-  }, [attachment.name]);
+    // BEGIN FILE READER
+    const file = attachment;
+    const reader = new FileReader();
+    // ? set up functional progress indicator
+    reader.addEventListener('progress', (val) => {
+      console.log('fr::PROGRESS: ', val);
+    }, true);
+
+    // once finished loading fileread, set image preview source
+    reader.addEventListener('load', () => {
+      imgRef.current.src = reader.result;
+    }, true);
+    if (file) {
+      reader.readAsDataURL(file);
+    }
+    // END FILE READER
+  }, [attachment]);
 
   // TODO implement removeAttachment
   const removeAttachment = () => {
